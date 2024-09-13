c1d8ae6e456acbb6169cc2dcf93fbb9297faf407
eslam <ealaa3764@gmail.com>
2024-09-13 17:55:07 +0200

    delettable21sq

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("hello saaaa ");
+  res.send("hello saataa ");
 });
 
 app.use("/api/admin", require("./routes/admin/auth"));
@@ -66,7 +66,7 @@ app.get("/api/users", async (req, res) => {
 // });
 
 client.connect().then(async () => {
-  //  await client.query("DROP TABLE IF EXISTS questiones;") 
+    await client.query("ALTER TABLE exams ADD COLUMN time INT ;") 
   //  await client.query("DROP TABLE IF EXISTS examresult;") 
   //  await client.query("DROP TABLE IF EXISTS exams;") 
   console.log("psql is connected ..");
